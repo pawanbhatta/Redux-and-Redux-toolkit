cbe97b28b6a8325b85ed827c9aac3c6d92e9c121
Pawan Bhatta <pawanbhatta00@gmail.com>
2022-06-02 13:27:57 +0545

    learnt about async thunks middleware in redux toolkit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
 const iceCreamActions =
   require("./features/icecream/iceCreamSlice").iceCreamActions;
+const { fetchUsers } = require("./features/user/userSlice");
 
 console.log("initial state : ", store.getState());
 
@@ -9,14 +10,16 @@ const unsubscribe = store.subscribe(() => {
   console.log("Updated state : ", store.getState());
 });
 
-store.dispatch(cakeActions.ordered(1));
-store.dispatch(cakeActions.ordered(1));
-store.dispatch(cakeActions.ordered(1));
-store.dispatch(cakeActions.restocked(3));
+store.dispatch(fetchUsers());
 
-store.dispatch(iceCreamActions.ordered(1));
-store.dispatch(iceCreamActions.ordered(1));
-store.dispatch(iceCreamActions.ordered(1));
-store.dispatch(iceCreamActions.restocked(3));
+// store.dispatch(cakeActions.ordered(1));
+// store.dispatch(cakeActions.ordered(1));
+// store.dispatch(cakeActions.ordered(1));
+// store.dispatch(cakeActions.restocked(3));
 
-unsubscribe();
+// store.dispatch(iceCreamActions.ordered(1));
+// store.dispatch(iceCreamActions.ordered(1));
+// store.dispatch(iceCreamActions.ordered(1));
+// store.dispatch(iceCreamActions.restocked(3));
+
+// unsubscribe();
